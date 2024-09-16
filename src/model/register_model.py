44c37ac98b4c848a2e80fd54ad072921d751c45d
pratik <pratik.sakode@gmail.com>
2024-09-16 16:12:09 +0530

    changed author name for dags hub auth

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -17,7 +17,7 @@ os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
-repo_owner = "pratiksakode"
+repo_owner = "pratik.sakode"
 repo_name = "mlops_mini_project"
 
 # Set up MLflow tracking URI
