e25d8e8c650869b5df547cc4701ea3531cd25235
pratik <pratik.sakode@gmail.com>
2024-09-21 05:52:45 +0530

    add version 3

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Login to my AWS ECR
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 529088281210.dkr.ecr.eu-north-1.amazonaws.com
-docker pull 529088281210.dkr.ecr.eu-north-1.amazonaws.com/mlops_ecr:latest
-docker stop my-app || true
-docker rm my-app || true
-docker run -d -p 80:5000 -e DAGSHUB_PAT=b3e227e91a236e7ae7ecd48d0b2cc4358b0cd41e --name campusx-app 529088281210.dkr.ecr.eu-north-1.amazonaws.com/mlops_ecr:latest
+docker pull 529088281210.dkr.ecr.eu-north-1.amazonaws.com/mlops_ecr:v3
+docker stop campusx-app || true
+docker rm campusx-app || true
+docker run -d -p 80:5000 -e DAGSHUB_PAT=b3e227e91a236e7ae7ecd48d0b2cc4358b0cd41e --name campusx-app 529088281210.dkr.ecr.eu-north-1.amazonaws.com/mlops_ecr:v3
